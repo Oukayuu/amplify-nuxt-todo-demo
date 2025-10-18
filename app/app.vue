92857c60b3643ffd971ab262fed40ef49de88b03
kosame <kayuuou@gmail.com>
2025-10-19 02:06:30 +0900

    一時保存

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,16 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+</script>
+
 <template>
-  <VitePwaManifest />
-  <NuxtPage />
+  <main>
+    <authenticator>
+      <template v-slot="{ signOut }">
+        <VitePwaManifest />
+        <NuxtPage />
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
+  </main>
 </template>
